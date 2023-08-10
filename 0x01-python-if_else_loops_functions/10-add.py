25c866bd4f45bdbf273ecec6862fde633b349627
Te125 <joyibom26@gmail.com>
2023-08-10 16:06:49 +0100

    python

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def add(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return a + b
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
+
 def uppercase(s):
     for char in s:
-        if 'a' <= char <= 'z':
-            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
-            print("{:s}".format(uppercase_char), end="")
+        if ord('a') <= ord(char) <= ord('z'):
+            print("{:c}".format(ord(char) - ord('a') + ord('A')), end="")
         else:
-            print("{:s}".format(char), end="")
-
-            print()
+            print("{:c}".format(ord(char)), end="")
+    print()
