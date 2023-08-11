c603a0057b7c267023b14d16e6093b31c6fab11d
Te125 <joyibom26@gmail.com>
2023-08-12 00:56:07 +0100

    import modules

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+
+exec("print('#pythoniscool')")
---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(argc))
+
+    for i, arg in enumerate(sys.argv[1:], 1):
+        print("{:d}: {:s}".format(i, arg))
