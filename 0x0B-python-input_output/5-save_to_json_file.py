81490a187ea556458669bc1fab3d2e2fa0f1fb85
Te125 <joyibom26@gmail.com>
2023-09-18 21:59:46 +0100

    update

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 def save_to_json_file(my_obj, filename):
     """ function definition """
     with open(filename, mode="w", encoding="utf-8") as file:
-        """ open and close the file with the with, 
+        """ open and close the file with the with,
         write to the file in write mode """
         json.dump(my_obj, file)
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,9 +9,8 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
 
     def to_json(self):
-        """ function definition that retrieves a dictionary 
+        """ function definition that retrieves a dictionary
         representation of student instance """
         return self.__dict__
