3ddebc574f30ed16a92c0070ae6ed508a3b244e5
Te125 <joyibom26@gmail.com>
2023-10-20 00:40:26 +0100

    update

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- cript that lists all shows, and all genres linked to that show, from the database
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title, IFNULL(tv_genres.name, NULL) AS genre
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
